debbd88a7bf7cb3ae60f218af22d1a53369d7dba
krishlala <71400038+krishlala@users.noreply.github.com>
2021-03-21 13:19:17 -0400

    Update mathgame.js

--- mathgame.js
+++ mathgame.js
@@ -15,29 +15,16 @@ document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_
 
 
 function send() {
-    get_question = document.getElementById("question").value;
-    question = get_question.toLowerCase();
-    console.log("question in lowercase = " + question);
-
-    charAt1 = question.charAt(1);
-    console.log(charAt1);
-
-    length_divide_2 = Math.floor(question.length_divide/2);
-    charAt2 = word.charAt(length_divide_2);
-    console.log(charAt2);
-
-    length_minus_1 = question.length - 1;
-    charAt3 = question.charAt(length_minus_1);
-    console.log(charAt3)
-
-    remove_charAt1 = question.replace(charAt1, "__");
-    remove_charAt2 = remove_charAt1.replace(charAt2, "__");
-    remove_charAt3 = remove_charAt2.replace(charAt3, "__");
-    console.log(remove_charAt3);
-
-    question_mulitply = "<h4 id='word_question'> Q.  "+remove_charAt3+"</h4>";
-    input_box = "<br> Answer : <input type='text' id='input_check_box'>";
-    row = question_multiply+input_box+check_button ;
-    document.getElementById("output").innerHTML = row;
-    document.getElementById("question").value = "";
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+    
+    question_number = "<h4>" + number + "X" + number2 + "</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row =  question_word + input_box + check_button ;
+    document.getElementById("output").innerHTML = "";
+    
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
 }
